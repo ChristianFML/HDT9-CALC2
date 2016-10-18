592ed79790bf85056f0d8a2cc65d3d31f838e24f
JoseLuisMendez <men15024@uvg.edu.gt>
2016-10-17 20:47:13 -0600

    Update and rename Diccionario.java to main.java

--- HDT9-CALC2/src/main.java
+++ HDT9-CALC2/src/main.java
@@ -3,17 +3,17 @@
  * Algoritmos y Estructuras de Datos
  * Ing. Douglas Barrios
  * Integrantes:
- *       Christian Morales - 15015
- *       José Luis Méndez - 1504
- * HDT 9  -> Diccionario 2
- * Fecha:  17 de octubre del 2016
+ *      Christian Morales - 15015
+ *      José Luis Méndez - 1504
+ * HDT 9 -> Diccionario 2
+ * Fecha: 17 de octubre del 2016
  */
 
 import java.util.*;
 import java.io.*;
 import javax.swing.*;
 
-public class Diccionario {
+public class main {
     
     static Map<String,String> map= null;
     static String words;
@@ -25,31 +25,21 @@ public class Diccionario {
         JOptionPane.showMessageDialog(null, "Ha escogido "+options[option]);
         
         //Implementation for Red Black BST
-        if(option == 1)
+        if(option == 1 || option == 2)
             map = new RedBlackBST();
         //Implementation for TreeMap
-        else if (option == 2)
+        else if (option == 3)
             map = new TreeMap<>();
    
         BufferedReader br = null;
         try {
-            br = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\diccionario.txt"));
+            br = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\diccionary.txt"));
             String linea;
             while((linea = br.readLine()) != null)
             {
-                linea = linea.replaceAll(",", ""); //esto sirve para eliminar las comas unidas a las palabras
+                linea = linea.replaceAll(",", "");
                 String[] palabras = linea.split("\\s{1,}");
-                if (palabras.length>2){
-                    if (palabras[1].equals("la") || palabras[1].equals("el")){
-                        map.put(palabras[0], palabras[2]);
-                    }
-                    else {
-                        map.put(palabras[0],palabras[1]);
-                    }
-                }
-                else if (palabras.length>1){
-                    map.put(palabras[0], palabras[1]);
-                }                
+                map.put(palabras[0], palabras[1]);
             }
         } catch (IOException e) {
                 e.printStackTrace();
@@ -61,7 +51,8 @@ public class Diccionario {
                 ex.printStackTrace();
             }
         }
-   
+        
+        JOptionPane.showMessageDialog(null, words);
    
    }
     
