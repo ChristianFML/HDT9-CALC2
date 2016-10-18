40f5106a84975c28541789b2eebe1235c945a026
JoseLuisMendez <men15024@uvg.edu.gt>
2016-10-17 20:41:57 -0600

    Update Diccionario.java

--- HDT9-CALC2/src/Diccionario.java
+++ HDT9-CALC2/src/Diccionario.java
@@ -3,10 +3,10 @@
  * Algoritmos y Estructuras de Datos
  * Ing. Douglas Barrios
  * Integrantes:
- *      Christian Morales - 15015
- *      José Luis Méndez - 1504
- * HDT 9 -> Diccionario 2
- * Fecha: 17 de octubre del 2016
+ *       Christian Morales - 15015
+ *       José Luis Méndez - 1504
+ * HDT 9  -> Diccionario 2
+ * Fecha:  17 de octubre del 2016
  */
 
 import java.util.*;
